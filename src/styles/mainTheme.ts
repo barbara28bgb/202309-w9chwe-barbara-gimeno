bc5be87c625bea84aebcc3ad9f042f789bc79fc4
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-11 14:22:54 +0100

    Add mainTheme

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  color: {
+    mainFont: "#541F03",
+    background: "#DEEDDE",
+    secondayFont: "#14585D",
+    footer: "#3B3F3B",
+  },
+  typography: {
+    mainFontFamily: "Lobster, sans-serif",
+    secondaryFontFamily: "Lobster Two, sans-serif",
+    titleSize: "2.5rem",
+    textSize: "1.7rem",
+    nputTextSize: "1.125rem",
+    inputTitleSize: "1.25rem",
+    footerFont: "Barlow Condensed, sans-serif",
+  },
+};
+
+export default mainTheme;
