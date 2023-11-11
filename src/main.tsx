4064cb49cc736ff99407152485bc9fd47ef69c66
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-11 20:03:52 +0100

    Add app to main

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,19 @@ import ReactDOM from "react-dom/client";
 import "@fontsource/lobster";
 import "@fontsource/lobster-two";
 import "@fontsource/barlow-condensed/100.css";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./styles/mainTheme";
+import GlobalStyle from "./styles/GlobalStyle";
+import App from "./App/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
+    <BrowserRouter>
+      <ThemeProvider theme={mainTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
