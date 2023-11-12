30ba051f8f8414355a1e97e53c2b99daefa8ee56
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-12 14:59:46 +0100

    Add test template

---
+++ src/components/ContactsList/ContactsList.test.tsx
@@ -0,0 +1,5 @@
+describe("Given a ContactsList compponent", () => {
+  describe("When it receives an array of contacts with 'Yastaqui Laguerra' and 'Tram Boliko'", () => {
+    test("Then it should showw 2 ContactsList, one with 'Yastaqui Laguerra' and 'Tram Bolico'", () => {});
+  });
+});
