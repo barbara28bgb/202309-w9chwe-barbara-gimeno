f991619935a172ab41ee757595fb8e8eb79f6e85
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-12 15:42:32 +0100

    Add contactsState: contactsReducer to index

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { contactsReducer } from "./feature/contactSlice/contactSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    contactsState: contactsReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
